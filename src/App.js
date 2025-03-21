d92028a265a9487d373803b775a28e31940370d5
Andoid <mosescodedev@gmail.com>
2025-03-21 04:29:42 +0100

    initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Djowda White Label Builder Frontend</h1>
+      <p>Welcome to your white-label builder platform!</p>
+    </div>
+  );
+}
+
+export default App;
